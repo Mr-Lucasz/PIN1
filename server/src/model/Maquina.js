3414c64fa8f7c0ceab50412888ebad5b98c304fb
BrenoJoseCoelho <93225993+BrenoJoseCoelho@users.noreply.github.com>
2023-05-21 15:49:44 -0300

    feito controller e model para maquina venda e funcionario, e adicionado rotas pertinente aos mesmo
    Feito para funcionario update e insert, para maquina feito update e insert, e para venda apenas insert

--- server/src/model/Maquina.js
+++ server/src/model/Maquina.js
@@ -2,7 +2,7 @@ const { supabase } = require('../database');
 
 
 exports.createMaquina = async (status_maquina, local_maquina) => {
-  const { maquina, error } = await supabase.from('Maquina').insert({ status_maquina, local_maquina}, { returning: 'minimal' });
+  const { maquina, error } = await supabase.from('Maquina').insert({status_maquina: status_maquina, local_maquina:local_maquina}, { returning: 'minimal' });
   if (error) {
     console.log('deu erro');
     console.log(error)
@@ -11,8 +11,7 @@ exports.createMaquina = async (status_maquina, local_maquina) => {
 };
 
 exports.updateMaquina = async (id_maquina,status_maquina, local_maquina) => {
-  const { id_maquina,status_maquina, local_maquina } = req.body;
-  const { maquina, error } = await supabase.from('Maquina').update({ status_maquina, local_maquina}).eq('id_maquina', id_maquina)
+  const { maquina, error } = await supabase.from('Maquina').update({ status_maquina: status_maquina, local_maquina:local_maquina}).eq('id_maquina', id_maquina)
   if (error) {
     console.log('deu erro');
     console.log(error)
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,7 +2,9 @@ const express = require('express'); // Importa o framework Express
 const router = express.Router(); // Cria uma instância do roteador do Express
 const authController = require('./controller/AuthController'); // Importa o controlador do usuário se for criar o controlador do usuário
 const bebidaController = require('./controller/BebidaController');
-
+const vendaController = require('./controller/VendaController');
+const funcionarioController = require('./controller/FuncionarioController');
+const maquinaController = require('./controller/MaquinaController');
 
 
 
@@ -21,6 +23,21 @@ router.post('/newbebida', bebidaController.createBebida);
 
 // Rota de atualiza bebida
 router.post('/updatebebida', bebidaController.updateBebida);
+
+//Rota criar venda
+router.post('/newvenda', vendaController.createVenda);
+
+//Rota criar maquina
+router.post('/newmaquina', maquinaController.createMaquina);
+
+// Rota de atualiza maquina
+router.post('/updatemaquina', maquinaController.updateMaquina);
+
+//Rota criar funcionario
+router.post('/newfuncionario', funcionarioController.createFuncionario);
+
+// Rota de atualiza funcionario
+router.post('/updatefuncionario', funcionarioController.updateFuncionario);
 
 module.exports = router;
 
