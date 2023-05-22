ece5025a304fc92c00ad2e0b39169bdeb9e2c20d
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-05-22 20:51:55 -0300

    subindo alterações

--- views/src/components/CadastroProdutoPage/CadastroProduto.jsx
+++ views/src/components/CadastroProdutoPage/CadastroProduto.jsx
@@ -34,7 +34,7 @@ function CadastroProduto() {
             <div className='inputs-field-form'>
               <div className='produto-field-form'>
                 <div className='label-produto'>
-                  <label htmlFor="produto">PRODUTO</label>
+                  <label htmlFor="produto">NOME PRODUTO</label>
                 </div>
                 <div className='input-produto'>
                   <input type="text"
@@ -43,18 +43,20 @@ function CadastroProduto() {
                     required />
                 </div>
               </div>
-              <div className='data-validade-field-form'>
-                <div className='label-data-validade'>
-                  <label htmlFor="data-validade">
-                    <span>DATA DE VALIDADE</span>
+              <div className='valor-field-form'>
+                <div className='label-valor'>
+                  <label htmlFor="valor-label">
+                    <span>VALOR</span>
                   </label>
                 </div>
 
-                <DatePicker
-                  selected={startDate}
-                  onChange={handleDateChange}
-                  showDayMonthYearPicker
-                  dateFormat="dd/MM/yyyy" className='datepicker' />
+                <div className='input-valor'>
+                  <input type="text"
+                    name="valor"
+                    placeholder="Informe o Preço:"
+                    required />
+                </div>
+
               </div>
 
               <div className='imagem-field-form'>
@@ -63,21 +65,26 @@ function CadastroProduto() {
 
                 </div>
                 <div className="input-imagem">
-                  <input type="file" accept="image/*" required />
-              
+                  <label for="arquivo">CARREGAR</label>
+                  <input type="file" name="arquivo" id="arquivo" />
                 </div>
 
               </div>
 
-              <div className='categoria-field-form'>
-                <div className='label-categoria'>
-                  <label htmlFor="categoria">CATEGORIA</label>
+
+              <div className='tipo-bebida-field-form'>
+                <div className='label-tipoBebida'>
+                  <label htmlFor="tipo-bebida">CATEGORIA</label>
                 </div>
-                <div className='input-categoria'>
+                <div className='input-bebida'>
                   <select value={selectedOption} onChange={handleSelectChange} required>
-                    <option value="opcao1">Categoria 1</option>
-                    <option value="opcao2">Categoria 2</option>
-                    <option value="opcao3">Categoria 3</option>
+                    <option value="refrigerante">Refrigerante</option>
+                    <option value="agua">Água</option>
+                    <option value="suco">Suco</option>
+                    <option value="energetico">Bebida Energética</option>
+                    <option value="cha">Chá</option>
+                    <option value="cafe">Café</option>
+                    <option value="alcoolica">Bebida Alcoólica</option>
                   </select>
                 </div>
               </div>
