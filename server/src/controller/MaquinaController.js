4831d844311915d7c0804dc690506c56cb01f919
João Parro <100303067+jparroj@users.noreply.github.com>
2023-05-23 11:18:52 -0300

    Correção Maquina e ajuste na reposição
    Arrumando funcoes para criar e atualizar as rotinas de maquina e reposicao

--- server/src/controller/MaquinaController.js
+++ server/src/controller/MaquinaController.js
@@ -7,27 +7,35 @@ exports.createMaquina = async (req, res) => {
   const {status_maquina, local_maquina } = req.body;
 
   try {
-   const maquina = await modelmaquina.createMaquina(status_maquina, local_maquina);
-
-   res.status(201).json({ 
-    message: 'Maquina criada com sucesso!', maquina });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json(
-      { message: 'Erro ao criar Maquina!' });
-  }
+   const { data, error } = await modelmaquina.createMaquina(status_maquina, local_maquina);
+   if (data && data.length) {
+    res.status(201).json({
+        message: 'Maquina criado com sucesso!', data
+    });
+} else {
+    throw Error('Erro ao criar maquina!')
+}
+} catch (error) {
+//Em caso de erro, retorna mensagem de erro
+console.log(error);
+res.status(500).json({ message: error.message });
+}
 };
 
 exports.updateMaquina = async (req, res) => {
     const {id_maquina,status_maquina, local_maquina} = req.body;
   
     try {
-     const bebida = await modelmaquina.updateMaquina(id_maquina,status_maquina, local_maquina);
-     res.status(201).json({ 
-      message: 'Maquina editada com sucesso!', maquina });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json(
-        { message: 'Erro ao editar a Maquina!' });
-    }
+     const { data, error } = await modelmaquina.updateMaquina(id_maquina,status_maquina, local_maquina);
+     if (data && data.length) {
+      res.status(201).json({
+          message: 'Maquina atualizado com sucesso!',data
+      });
+  } else {
+      throw Error('Erro ao atualizar maquina!')
+  }
+} catch (error) {
+  console.log(error);
+  res.status(500).json({ message: error.message });
+}
   };
