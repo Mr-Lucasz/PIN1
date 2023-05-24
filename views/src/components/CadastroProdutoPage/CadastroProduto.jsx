0edf05799216e98817855a821b1adb58a0d62faa
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-05-24 01:04:08 -0300

    modal add Estoque

--- views/src/components/CadastroProdutoPage/CadastroProduto.jsx
+++ views/src/components/CadastroProdutoPage/CadastroProduto.jsx
@@ -1,107 +1,106 @@
-import React, { useState } from 'react';
-import Header from '../Header/Header';
-import iconeVoltar from '../util/iconeVoltar.png';
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import iconeVoltar from "../util/iconeVoltar.png";
 import "react-datepicker/dist/react-datepicker.css";
-import './CadastroProdutoStyle.css';
-import axios from 'axios';//biblioteca para fazer requisições HTTP em JavaScript
-import PopupFormEstoque from './PopupFormEstoque';
-
-
+import "./CadastroProdutoStyle.css";
+import axios from "axios"; //biblioteca para fazer requisições HTTP em JavaScript
+import Modal from "react-modal";
 
 function CadastroProduto() {
+  const [nome_bebida, setNomeBebida] = useState("");
+  const [valor_bebida, setValorBebida] = useState("");
+  const [imagem_bebida, setImagemBebida] = useState("");
+  const [tipo_bebida, setTipoBebida] = useState("");
 
-  const [nome_bebida, setNomeBebida] = useState('');
-  const [valor_bebida, setValorBebida] = useState('');
-  const [imagem_bebida, setImagemBebida] = useState('');
-  const [tipo_bebida, setTipoBebida] = useState('');
-   const [exibirPopup, setExibirPopup] = useState(false);
-
+  const [exibirModal, setExibirModal] = useState(false);
 
   const createProduct = async (e) => {
     e.preventDefault();
-    const response = await axios.post('http://localhost:3001/newbebida', {
+    const response = await axios.post("http://localhost:3001/newbebida", {
       nome_bebida,
       tipo_bebida,
-      valor_bebida
+      valor_bebida,
       // imagemBebida,
-    });      
+    });
 
     // Exibe no console o objeto de resposta da requisição
     console.log(response.data);
-  }
-
-
-
+  };
 
   return (
     <div>
       <Header />
       <div className="cadastro-produto-wrap">
         <div className="title-cadastro-voltar" href="/home">
-
           <a href="/home" className="titulo">
-            <img src={iconeVoltar} alt="Ícone de voltar" className="title-icon" />
+            <img
+              src={iconeVoltar}
+              alt="Ícone de voltar"
+              className="title-icon"
+            />
             <span>CADASTRAR PRODUTO</span>
           </a>
         </div>
         <form className="container-cadastro-produto">
-          <div className='container-form'>
-            <div className='inputs-field-form'>
-              <div className='produto-field-form'>
-                <div className='label-produto'>
+          <div className="container-form">
+            <div className="inputs-field-form">
+              <div className="produto-field-form">
+                <div className="label-produto">
                   <label htmlFor="produto">NOME PRODUTO</label>
                 </div>
-                <div className='input-produto'>
-                  <input type="text"
+                <div className="input-produto">
+                  <input
+                    type="text"
                     name="produto"
                     placeholder="Informe o Produto:"
                     required
-                    onChange={(e) =>
-                      setNomeBebida(e.target.value)} />
+                    onChange={(e) => setNomeBebida(e.target.value)}
+                  />
                 </div>
               </div>
-              <div className='valor-field-form'>
-                <div className='label-valor'>
+              <div className="valor-field-form">
+                <div className="label-valor">
                   <label htmlFor="valor-label">
                     <span>VALOR</span>
                   </label>
                 </div>
 
-                <div className='input-valor'>
-                  <input type="number"
+                <div className="input-valor">
+                  <input
+                    type="number"
                     name="valor"
                     step="0.01"
                     placeholder="Informe o Preço:"
                     required
-                    onChange={(e) =>
-                      setValorBebida(e.target.value)} />
+                    onChange={(e) => setValorBebida(e.target.value)}
+                  />
                 </div>
-
               </div>
 
-              <div className='imagem-field-form'>
-                <div className='label-imagem'>
+              <div className="imagem-field-form">
+                <div className="label-imagem">
                   <label htmlFor="imagem">IMAGEM</label>
-
                 </div>
                 <div className="input-imagem">
                   <label for="arquivo">CARREGAR</label>
-                  <input type="file" name="arquivo" id="arquivo"
+                  <input
+                    type="file"
+                    name="arquivo"
+                    id="arquivo"
                     required
-                    onChange={(e) =>
-                      setImagemBebida(e.target.value)} />
+                    onChange={(e) => setImagemBebida(e.target.value)}
+                  />
                 </div>
               </div>
-              <div className='tipo-bebida-field-form'>
-                <div className='label-tipoBebida'>
+              <div className="tipo-bebida-field-form">
+                <div className="label-tipoBebida">
                   <label htmlFor="tipo-bebida">CATEGORIA</label>
                 </div>
-                <div className='input-bebida'>
-
-                  <select value={tipo_bebida}
+                <div className="input-bebida">
+                  <select
+                    value={tipo_bebida}
                     required
-                    onChange={(e) =>
-                      setTipoBebida(e.target.value)}>
+                    onChange={(e) => setTipoBebida(e.target.value)}>
                     <option value="refrigerante">Refrigerante</option>
                     <option value="agua">Água</option>
                     <option value="suco">Suco</option>
@@ -110,36 +109,66 @@ function CadastroProduto() {
                     <option value="cafe">Café</option>
                     <option value="alcoolica">Bebida Alcoólica</option>
                   </select>
-
                 </div>
               </div>
-
             </div>
-            <div className='area-button-confirmar'>
-              <div className='area-btn-form'>
-                <button type="submit"
-                  className='confirmar-button'
-                  onClick={(e) =>
-                    createProduct(e)}>CONFIRMAR</button>
+            <div className="area-button-confirmar">
+              <div className="area-btn-form">
+                <button
+                  type="submit"
+                  className="confirmar-button"
+                  onClick={(e) => createProduct(e)}
+                >
+                  CONFIRMAR
+                </button>
               </div>
               {/* <input type="submit" value="CONFIRMAR" className="confirmar-button" /> */}
             </div>
           </div>
         </form>
-        <div className='area-btn-acessar'>
-                <button type="button"
-                  className='acessar-estoque-button'
-                  onClick={() => setExibirPopup(true)}
-                  >ACESSAR ESTOQUE</button>
-              </div>    
-              {exibirPopup && PopupFormEstoque}
+        <div className="area-btn-acessar">
+          <button
+            onClick={() => setExibirModal(true)}
+            className="acessar-estoque-button"
+          >
+            ACESSAR ESTOQUE
+          </button>
+          <Modal
+            isOpen={exibirModal}
+            onRequestClose={() => setExibirModal(false)}
+            contentLabel="Exemplo de Modal"
+            overlayClassName="modal-overlay"
+            className="modal-content"
+            ariaHideApp={false}
+          >
+           
+            <form className="form-modal">
+            <h2>ESTOQUE</h2>
+              <div className="escolher-bebida">
+                <label htmlFor="bebidas-disponiveis">Bebidas Disponíveis</label>
+                <select
+                  id="bebidasDisponiveis"
+                  name="bebidasDisponiveis"
+                  classeName="bebidas-disponiveis"
+                >
+                  <option value="">Selecione uma bebida</option>
+                  <option value="refrigerante">Refrigerante</option>
+                  <option value="agua">Água</option>
+                  <option value="suco">Suco</option>
+                  <option value="energetico">Bebida Energética</option>
+                  <option value="cha">Chá</option>
+                  <option value="cafe">Café</option>
+                  <option value="alcoolica">Bebida Alcoólica</option>
+                </select>
+              </div>
+              <button>INSERIR NO ESTOQUE</button>
+            </form>
+
+            <button onClick={() => setExibirModal(false)}>Fechar</button>
+          </Modal>
+        </div>
       </div>
-
-
-
     </div>
-
-
   );
 }
 
--- views/src/components/CadastroProdutoPage/PopupFormEstoque.jsx
+++ views/src/components/CadastroProdutoPage/PopupFormEstoque.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import Header from '../Header/Header';
+
+import Modal from 'react-modal';
 
 
 function PopupFormEstoque() {
@@ -14,20 +15,30 @@ function PopupFormEstoque() {
       setIsPopupOpen(false);
     };
 
-  return (
-    <div>
-      <Header />
-      <div className='popup-form'>   
-        <div className='popup-conteudo'>
-        <h3>Título do PopUp</h3>
-        <p>Conteúdo do PopUp</p>
-        <button className="fechar-popup">Fechar</button>
-            </div>
-            </div>
-
-
-    </div>
-  );
+    return (
+      <div className="Container">
+        <button onClick={isPopupOpen}>Open Modal</button>
+        <Modal
+          isOpen={exibirPopup}
+          onRequestClose={fecharPopup}
+          contentLabel="Example Modal"
+          overlayClassName="modal-overlay"
+          className="modal-content"
+        >
+          <h2>Hello - I am a modal!</h2>
+          <hr />
+          <p>
+            We maintain that accessibility is a key component of any modern web
+            application. As such, we have created this modal in such a way that it
+            fulfills the accessibility requirements of the modern web. We seek to
+            keep the focus on accessibility while providing a functional, capable
+            modal component for general use.
+          </p>
+          <button onClick={fecharPopup}>Close</button>
+        </Modal>
+      </div>
+    );
+  
 }
 
 export default PopupFormEstoque;
