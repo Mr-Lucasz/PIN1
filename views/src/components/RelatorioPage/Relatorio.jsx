fa3b94160b2834a0622f204f449c8a9bf0789755
João Parro <100303067+jparroj@users.noreply.github.com>
2023-05-24 20:29:45 -0300

    finaleira
    SOCORRO

--- views/src/components/RelatorioPage/Relatorio.jsx
+++ views/src/components/RelatorioPage/Relatorio.jsx
@@ -1,28 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import './RelatorioStyle.css';
-import axios from 'axios';//biblioteca para fazer requisições HTTP em JavaScript
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+
+
 
 function Relatorio() {
   const [id_totem, settotem] = useState('');
   const [selecttotem, setSelectTotem] = useState([]);
   const [dataIni, setDataIni] = useState('');
   const [dataFim, setDataFim] = useState('');
+  const [selectRelatorio, setSelectRelatorio] = useState([]);
 
   const [resultadoTabel, setTable] = useState([]);
 
   const pesquisaRelatorio = async (e) => {
     e.preventDefault();
-    const {data, error} = await axios.post('http://localhost:3001/pesquisarelatorio', {
+    const { data, error } = await axios.post('http://localhost:3001/pesquisarelatorio', {
       id_totem,
       dataIni,
       dataFim
-    });
+    }).then((data) =>{ console.log(data.data.data) ;
+      if (Array.isArray(data.data.data)) {
+      setTable(data.data.data);
+    } else {
+      setTable([]);
+    } });
 
-    setTable(data);   
+   
     
   }
+  
+  
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -34,18 +45,27 @@ function Relatorio() {
   const [showCalendar, setShowCalendar] = useState(false);
 
   useEffect(() => {
-
     const selectTotem = async () => {
       try {
-        const data = await axios.get('http://localhost:3001/selecttotem');
-        
-        setSelectTotem(data.data.data); 
+        const { data } = await axios.get('http://localhost:3001/selecttotem');
+        setSelectTotem(data.data);
       } catch (error) {
         console.error('Erro ao buscar os dados do banco:', error);
       }
     };
     selectTotem();
+  }, []);
 
+  useEffect(() => {
+    const selectRelatorio = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:3001/pesquisarelatorio');
+        setSelectTotem(data.data);
+      } catch (error) {
+        console.error('Erro ao buscar os dados do banco:', error);
+      }
+    };
+    selectRelatorio();
   }, []);
 
   return (
@@ -54,42 +74,43 @@ function Relatorio() {
       <div className='page-relatorio'>
         <div className='titulo-pagina'>
           <a href="/home" className="titulo">
-              &#10094; RELATÓRIO
+            &#10094; RELATÓRIO
           </a>
         </div>
-        
+
         <form className='form-relatorio'>
 
-        <br/>
+          <br />
           <div className='id_maquina'>
             <label className='maquina'>Totem:</label>
-            <select className='opcao-maquina'
-             value={id_totem}
-             required
-             onChange={(e) =>
-              settotem(e.target.value)}>
-            <option value="">Selecione um Totem</option>
-            {selecttotem.map((totem) => (
-            <option key={totem.id_totem} value={totem.id_totem}>{totem.nome_totem}</option>
-            ))}
+            <select
+              className='opcao-maquina'
+              value={id_totem}
+              required
+              onChange={(e) => settotem(e.target.value)}
+            >
+              <option value="">Selecione um Totem</option>
+              {selecttotem.map((totem) => (
+                <option key={totem.id_totem} value={totem.id_totem}>{totem.nome_totem}</option>
+              ))}
             </select>
           </div>
 
-          <br/>
+          <br />
           <div className='data'>
             <label htmlFor='data'>Data</label>
-            
-            <br/>
+
+            <br />
             <input
-            className='preencherdata'
+              className='preencherdata'
               type='date'
               id='data'
               value={dataIni}
               onChange={(e) => setDataIni(e.target.value)}
             />
-            <br/>
+            <br />
             <label htmlFor='data'>Até</label>
-            <br/>
+            <br />
             <input
               className='preencherdata'
               type='date'
@@ -99,43 +120,37 @@ function Relatorio() {
             />
           </div>
 
+          <br />
           
-
-          <br/>
-
+          <button className='botao-confirmar' type='submit' onClick={pesquisaRelatorio}>
+            Confirmar
+          </button>
           
-          <button className='botao-confirmar' type='submit'  onClick={(e) => pesquisaRelatorio(e)}>Confirmar</button>
-         
-
-          <br/>
+          <br />
 
         </form>
+        <div className="resultado-tabela">
+          <h2>Resultado da Consulta</h2>
+          <ul>
+            
+            {resultadoTabel.map((item) => (
+              <>
 
-        <div className="tablediv">
-        
-       
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          {/* Outras colunas da tabela */}
-        </tr>
-      </thead>
-      <tbody>
-        {console.log(resultadoTabel) }
-          <tr >
-            <td></td>
-            {/* Renderizar outras colunas da tabela */}
-          </tr>
-        
-      </tbody>
-    </table>
- 
-  
+              <li key={item.id_venda}>{item.Totem.nome_totem}</li>
+              <li key={item.id_venda}>{item.id_venda}</li>
+             
+              <li key={item.id_venda}>{item.data_venda}</li>
+             
+              
+              <br></br>
+              </>
+              
+            ))}
+          </ul>
         </div>
+        
       </div>
     </div>
-
   );
 }
 
