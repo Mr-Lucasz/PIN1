d906c78a52b0b48843ae94f5946a0e0d4f987c84
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-06-21 00:53:31 -0300

    img

--- PinkDrinkers-Totem/views/src/pages/Homepage/Homepage.jsx
+++ PinkDrinkers-Totem/views/src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from "./Homepage.module.css";
+import {TitleApp} from "../../components/TitleApp";
+import Logo from "../../assets/img/Logo.svg";
+import Cart from "../../assets/img/cart.svg";
 
 export function HomePage() {
 
 
   return (
     <div className={styles.wrapperHome} >
+
+    <header>
+    <div className={styles.imgRefri}>
+   <img src={Logo} alt="Logotipo do Ignite" />
+   <img src={Cart} alt="Logotipo do Ignite" />
+    </div>
+      </header>
      <h1>HELLO WORLD</h1>
+     <TitleApp title="PRODUTOS" />
       </div>
   );
 }
